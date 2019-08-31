9c4a7636498e7247a3e49ee210e0f3cea14177bb
Danila <Danila00000@Gmail.com>
2019-08-31 14:13:06 +0700

    completed all setup of new project

--- dev/main.js
+++ dev/main.js
@@ -1,8 +1,3 @@
-//import $ from 'jquery';
-import avg from './blocks/common.blocks/common/some.js';
-
-// console.log(avg(1, 4, 8));
-
 console.log("Webpack work very well");
 
 import './style.styl';
--- webpack.config.js
+++ webpack.config.js
@@ -13,137 +13,141 @@ console.log('DEV_MODE=' + DEV_MODE);
 console.log('PROD_MODE=' + PROD_MODE);
 
 const PATHS = {
-  dev: path.join(__dirname, 'dev/'),
-  prod: path.join(__dirname, 'prod/')
+	dev: path.join(__dirname, 'dev/'),
+	prod: path.join(__dirname, 'prod/')
 };
 
 let conf = {
-  entry: {
-    main: PATHS.dev + "main.js",
-  },
-  output: {
-    path: path.resolve(__dirname, PATHS.prod),
-    filename: "js/[name].bundle.js"
-  },
-  devServer: {
-  	overlay: true,
-    contentBase: path.join(__dirname, 'prod'),
-    compress: true,
-    host: 'localhost',
-    port: 8080,
-    open: true
-  },
-    module: {
-    rules: [
-      {
-        test: /\.js$|\.es6$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              "env",
-              "stage-3"
-            ]
-          }
-        }
-      },
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader',
-        options: {
-          pretty: true //PROD_MODE ? false : true
-        }
-      },
-      {
-        test: /\.styl(us)?$/,
-        include: path.resolve(__dirname, 'dev'),
-        use: ExtractTextPlugin.extract({
-          use: [
-            // {
-            //   loader: "style-loader" // creates style nodes from JS strings
-            // },
-            {
-              loader: "css-loader" // translates CSS into CommonJS
-            },
-            {
-              loader: "stylus-loader" // compiles Stylus to CSS
-            }
-          ]
-        })
-      },
-      //работа с изображениеми
-       {
-        test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.ico$|\.mp3$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name].[ext]',
-              outputPath: (file) => {
-                let path = file.split("dev/")[1]; //this will yield assets
-                let newPath = '../' + path;
-                return newPath
-              }
-            },  
-          },
-          'img-loader',
-        ]
-      },
-      //работа со шрифтами
-      {
-        test: /\.(eot|otf|svg|ttf|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name].[ext]',
-              outputPath: (file) => {
-                let path = file.split("dev/")[1];  
-                let newPath = '../' + path;  
-                return newPath
-              }
-            }  
-          },
-        ]
-      },
-    ]
-  },
-  plugins: [
-  PROD_MODE ? new CleanWebpackPlugin(['prod'])
-  : () => {},
-  new ExtractTextPlugin({
-      filename: 'css/style.css',
-      allChunks: true,
-    }),
-  new HtmlWebpackPlugin({
-    hash: true,
-    template: PATHS.dev + 'index.pug',
-    // inject: false, // отключает автоматическую ставку ссылок на js и css файлы в HTML 
-    }),
-    // new HtmlWebpackPlugin({ // необходимо создать экземпляр 1 стр. html = 1 экземпляр
-        //  filename: 'test.html',
-        //  template: PATHS.dev + 'pug/test.pug',
-        // }),
-  new CopyWebpackPlugin([{
-        from: './dev/fonts',
-        to: './fonts'
-      },
-      {
-        from: './dev/img',
-        to: './img'
-      }
-    ]),
-  PROD_MODE ? new ImageminPlugin({
-    test: /\.(png|jpe?g|gif|ico|svg)$/i
-  })
-  : () => {},
-  ]
+	entry: {
+		main: PATHS.dev + "main.js",
+	},
+	output: {
+		path: path.resolve(__dirname, PATHS.prod),
+		filename: "js/[name].bundle.js"
+	},
+	devServer: {
+		overlay: true,
+		contentBase: path.join(__dirname, 'prod'),
+		compress: true,
+		host: 'localhost',
+		port: 8080,
+		open: true
+	},
+		module: {
+		rules: [
+			{
+				test: /\.js$|\.es6$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							"env",
+							"stage-3"
+						]
+					}
+				}
+			},
+			{
+				test: /\.pug$/,
+				loader: 'pug-loader',
+				options: {
+					pretty: true //PROD_MODE ? false : true
+				}
+			},
+			{
+				test: /\.styl(us)?$/,
+				include: path.resolve(__dirname, 'dev'),
+				use: ExtractTextPlugin.extract({
+					use: [
+						// {
+						//   loader: "style-loader" // creates style nodes from JS strings
+						// },
+						{
+							loader: "css-loader" // translates CSS into CommonJS
+						},
+						{
+							loader: "stylus-loader" // compiles Stylus to CSS
+						}
+					]
+				})
+			},
+			//работа с изображениеми
+			 {
+				test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.ico$|\.mp3$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[path][name].[ext]',
+							outputPath: (file) => {
+								let path = file.split("dev/")[1]; //this will yield assets
+								let newPath = '../' + path;
+								return newPath
+							}
+						},  
+					},
+					'img-loader',
+				]
+			},
+			//работа со шрифтами
+			{
+				test: /\.(eot|otf|svg|ttf|woff|woff2)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[path][name].[ext]',
+							outputPath: (file) => {
+								let path = file.split("dev/")[1];  
+								let newPath = '../' + path;  
+								return newPath
+							}
+						}  
+					},
+				]
+			},
+		]
+	},
+	plugins: [
+	PROD_MODE ? new CleanWebpackPlugin(['prod'])
+	: () => {},
+	new ExtractTextPlugin({
+			filename: 'css/style.css',
+			allChunks: true,
+		}),
+	new HtmlWebpackPlugin({
+		hash: true,
+		template: PATHS.dev + 'index.pug',
+		// inject: false, // отключает автоматическую ставку ссылок на js и css файлы в HTML 
+		}),
+		// new HtmlWebpackPlugin({ // необходимо создать экземпляр 1 стр. html = 1 экземпляр
+				//  filename: 'test.html',
+				//  template: PATHS.dev + 'pug/test.pug',
+				// }),
+	new CopyWebpackPlugin([{
+				from: './dev/img',
+				to: './img'
+			},
+			{
+				from: './dev/blocks/common.blocks/common/css',
+				to: './css'
+			},
+			{
+				from: './dev/blocks/common.blocks/common/js',
+				to: './js'
+			}
+		]),
+	PROD_MODE ? new ImageminPlugin({
+		test: /\.(png|jpe?g|gif|ico|svg)$/i
+	})
+	: () => {},
+	]
 };
 
 module.exports = (env, options) => {
-  let mode = options.mode ==='production';
-  conf.devtool = mode ?  false //'source-map' 
-            : 'eval-sourcemap';
-  return conf;
+	let mode = options.mode ==='production';
+	conf.devtool = mode ?  false //'source-map' 
+						: 'eval-sourcemap';
+	return conf;
 };
