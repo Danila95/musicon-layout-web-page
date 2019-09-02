2d51f6044bdb02025be61a01645bef0a334bb23b
Danila <Danila00000@Gmail.com>
2019-09-02 09:02:38 +0700

    started made up parallax effect

--- dev/main.js
+++ dev/main.js
@@ -2,4 +2,96 @@ console.log("Webpack work very well");
 
 import './style.styl';
 
+$(function () {
 
+    // Preloader
+    // var $preloader = $('#page-preloader'),
+    //     $spinner   = $preloader.find('.spinner');
+    // $spinner.fadeOut();
+    // $preloader.delay(350).fadeOut('slow');
+
+    /* Параллакс от движения мыши */
+    if ($(window).width() > 960)
+    {
+        $('body').parallax({
+            'elements': [
+                {
+                    'selector': '.slider__ellips',
+                    'properties': {
+                        'x': {
+                            'right': {
+                                'initial': 0,
+                                'multiplier': 0.002,
+                                'unit': '%',
+                                'invert': false
+                            }
+                        },
+                        'y': {
+                            'top': {
+                                'initial': -10,
+                                'multiplier': 0.002,
+                                'unit': '%',
+                                'invert': true
+                            }
+                        }
+                    }
+                },
+                {
+                    'selector': '.slider__lines-dot',
+                    'properties': {
+                        'x': {
+                            'left': {
+                                'initial': -14,
+                                'multiplier': 0.0009,
+                                'unit': '%',
+                                'invert': false
+                            }
+                        },
+                        'y': {
+                            'bottom': {
+                                'initial': -10,
+                                'multiplier': 0.0009,
+                                'unit': '%',
+                                'invert': true
+                            }
+                        }
+                    }
+                },
+                {
+                    'selector': '.slider__plus-yellow',
+                    'properties': {
+                        'x': {
+                            'left': {
+                                'initial': 30,
+                                'multiplier': 0.0009,
+                                'unit': '%',
+                                'invert': false
+                            }
+                        },
+                        'y': {
+                            'top': {
+                                'initial': -40,
+                                'multiplier': 0.0009,
+                                'unit': '%',
+                                'invert': true
+                            }
+                        }
+                    }
+                },
+                {
+                    'selector': '.slider__rnb-man',
+                    'properties': {
+                        'x': {
+                            'left': {
+                                'initial': 9,
+                                'multiplier': 0.0003,
+                                'unit': '%',
+                                'invert': false
+                            }
+                        }
+                    }
+                }
+            ]
+        });
+    };
+});
