fb5449593285e90dd912abc8895afa33aa261034
Danila <Danila00000@Gmail.com>
2019-09-03 08:46:42 +0700

    Complited made up element container__slider

--- dev/main.js
+++ dev/main.js
@@ -1,97 +1,150 @@
-console.log("Webpack work very well");
-
 import './style.styl';
 
 $(function () {
 
-    // Preloader
-    // var $preloader = $('#page-preloader'),
-    //     $spinner   = $preloader.find('.spinner');
-    // $spinner.fadeOut();
-    // $preloader.delay(350).fadeOut('slow');
+	// Preloader
+	// var $preloader = $('#page-preloader'),
+	//     $spinner   = $preloader.find('.spinner');
+	// $spinner.fadeOut();
+	// $preloader.delay(350).fadeOut('slow');
 
-    /* Параллакс от движения мыши */
-    if ($(window).width() > 960)
-    {
-        $('body').parallax({
-            'elements': [
-                {
-                    'selector': '.slider__ellips',
-                    'properties': {
-                        'x': {
-                            'right': {
-                                'initial': 0,
-                                'multiplier': 0.002,
-                                'unit': '%',
-                                'invert': false
-                            }
-                        },
-                        'y': {
-                            'top': {
-                                'initial': -10,
-                                'multiplier': 0.002,
-                                'unit': '%',
-                                'invert': true
-                            }
-                        }
-                    }
-                },
-                {
-                    'selector': '.slider__lines-dot',
-                    'properties': {
-                        'x': {
-                            'left': {
-                                'initial': -14,
-                                'multiplier': 0.0009,
-                                'unit': '%',
-                                'invert': false
-                            }
-                        },
-                        'y': {
-                            'bottom': {
-                                'initial': -10,
-                                'multiplier': 0.0009,
-                                'unit': '%',
-                                'invert': true
-                            }
-                        }
-                    }
-                },
-                {
-                    'selector': '.slider__plus-yellow',
-                    'properties': {
-                        'x': {
-                            'left': {
-                                'initial': 30,
-                                'multiplier': 0.0009,
-                                'unit': '%',
-                                'invert': false
-                            }
-                        },
-                        'y': {
-                            'top': {
-                                'initial': -40,
-                                'multiplier': 0.0009,
-                                'unit': '%',
-                                'invert': true
-                            }
-                        }
-                    }
-                },
-                {
-                    'selector': '.slider__rnb-man',
-                    'properties': {
-                        'x': {
-                            'left': {
-                                'initial': 9,
-                                'multiplier': 0.0003,
-                                'unit': '%',
-                                'invert': false
-                            }
-                        }
-                    }
-                }
-            ]
-        });
-    };
+	/* Параллакс от движения мыши */
+	if ($(window).width() > 960)
+	{
+		$('body').parallax({
+			'elements': [
+				{
+					'selector': '.slider__ellips',
+					'properties': {
+						'x': {
+							'right': {
+								'initial': 0,
+								'multiplier': 0.002,
+								'unit': '%',
+								'invert': false
+							}
+						},
+						'y': {
+							'top': {
+								'initial': -10,
+								'multiplier': 0.002,
+								'unit': '%',
+								'invert': true
+							}
+						}
+					}
+				},
+				{
+					'selector': '.slider__lines-dot',
+					'properties': {
+						'x': {
+							'left': {
+								'initial': -14,
+								'multiplier': 0.0009,
+								'unit': '%',
+								'invert': false
+							}
+						},
+						'y': {
+							'bottom': {
+								'initial': -10,
+								'multiplier': 0.0009,
+								'unit': '%',
+								'invert': true
+							}
+						}
+					}
+				},
+				{
+					'selector': '.slider__plus-yellow',
+					'properties': {
+						'x': {
+							'left': {
+								'initial': 30,
+								'multiplier': 0.0009,
+								'unit': '%',
+								'invert': false
+							}
+						},
+						'y': {
+							'top': {
+								'initial': -40,
+								'multiplier': 0.0009,
+								'unit': '%',
+								'invert': true
+							}
+						}
+					}
+				},
+				{
+					'selector': '.slider__rnb-man',
+					'properties': {
+						'x': {
+							'left': {
+								'initial': 9,
+								'multiplier': 0.0003,
+								'unit': '%',
+								'invert': false
+							}
+						}
+					}
+				},
+				{
+					'selector': '.slider__plus-dots',
+					'properties': {
+						'x': {
+							'right': {
+								'initial': 0,
+								'multiplier': 0.003,
+								'unit': '%',
+								'invert': false
+							}
+						}
+					}
+				},
+				{
+					'selector': '.slider__triangles',
+					'properties': {
+						'x': {
+							'right': {
+								'initial': 10,
+								'multiplier': 0.0012,
+								'unit': '%',
+								'invert': false
+							}
+						},
+						'y': {
+							'top': {
+								'initial': -20,
+								'multiplier': 0.0012,
+								'unit': '%',
+								'invert': true
+							}
+						}
+					}
+				},
+				{
+					'selector': '.slider__line-wave',
+					'properties': {
+						'x': {
+							'right': {
+								'initial': 10,
+								'multiplier': 0.004,
+								'unit': '%',
+								'invert': false
+							}
+						},
+						'y': {
+							'top': {
+								'initial': -20,
+								'multiplier': 0.004,
+								'unit': '%',
+								'invert': true
+							}
+						}
+					}
+				}
+			]
+		});
+	};
 });
